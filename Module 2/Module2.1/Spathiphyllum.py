1d87b4cac86483f8980410e9b724660fa95140e8
= <=>
2024-03-10 11:53:41 +0530

    committing Spathiphyllum from module 2.1

---
+++ Module 2/Module2.1/Spathiphyllum.py
@@ -0,0 +1,10 @@
+name = input("Enter the flower name : ")
+if name == "Spathiphyllum":
+    print("Spathiphyllum is the best plant ever!")
+elif name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not", name +"!")
+
+
+    
